355194c516019e2f54af93a4bf1393664774f386
Pablo Cornejo <pcornejo@icloud.com>
2021-05-07 08:52:27 -0400

    Implement custom init for Episode to remove additional whitespaces

--- BreakingBadCompanion/Models/Episode.swift
+++ BreakingBadCompanion/Models/Episode.swift
@@ -23,6 +23,27 @@ struct Episode: Codable {
     let title, season, airDate: String
     let characters: [String]
     let episode, series: String
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        try episodeId = container.decode(Int.self, forKey: .episodeId)
+        try title = container.decode(String.self, forKey: .title)
+        try season = container.decode(String.self, forKey: .season).trimmingCharacters(in: .whitespaces) // Custom init implemented to correct this
+        try airDate = container.decode(String.self, forKey: .airDate)
+        try characters = container.decode([String].self, forKey: .characters)
+        try episode = container.decode(String.self, forKey: .episode)
+        try series = container.decode(String.self, forKey: .series)
+    }
+    
+    init(episodeId: Int, title: String, season: String, airDate: String, characters: [String], episode: String, series: String) {
+        self.episodeId = episodeId
+        self.title = title
+        self.season = season
+        self.airDate = airDate
+        self.characters = characters
+        self.episode = episode
+        self.series = series
+    }
 }
 
 extension Episode: Identifiable {
